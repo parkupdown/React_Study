eb9caf386282b7a4c28e9e9ef64404c72453b3e8
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-01-31 15:09:04 +0900

    UseEffect 복습

--- react-for-recap/src/App.js
+++ react-for-recap/src/App.js
@@ -1,5 +1,34 @@
+import Button from "./Button";
+import styled from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App">hahaha</div>;
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((current) => current + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  console.log("i run all the time");
+
+  const iRunOnlyOnce = () => console.log("Call an api");
+  useEffect(iRunOnlyOnce, [counter]);
+  const iSearchSomething = () => console.log("Searching");
+  useEffect(iSearchSomething, [keyword]);
+
+  return (
+    <div className="App">
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;
