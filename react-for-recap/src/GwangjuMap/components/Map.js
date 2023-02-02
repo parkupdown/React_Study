eea39ca51490dad62e114dfa662211fefc17a781
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-02-02 20:04:37 +0900

    refactor: 지도의 크기를 제한하도록 수정

--- react-for-recap/src/GwangjuMap/components/Map.js
+++ react-for-recap/src/GwangjuMap/components/Map.js
@@ -11,12 +11,14 @@ const Map = () => {
     };
 
     const map = new kakao.maps.Map(container, options);
+    map.setMinLevel(7);
+    map.setMaxLevel(8);
   }, []);
 
   return (
     <div
       id="map"
-      style={{ width: "500px", height: "400px", borderRadius: "30px" }}
+      style={{ width: "600px", height: "400px", borderRadius: "30px" }}
     ></div>
   );
 };
