7459f5ec83fad03a0acb609ef7a3137235f096ad
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-02-06 14:04:29 +0900

    feat: 클릭한 장소에 대한 상세정보를 보여주는 기능 추가

--- react-for-recap/src/GwangjuMap/routes/Spot.js
+++ react-for-recap/src/GwangjuMap/routes/Spot.js
@@ -2,14 +2,18 @@ import { useLocation, useParams } from "react-router-dom";
 
 const Spot = () => {
   const { spot } = useParams();
-  const location = useLocation();
-  console.log(spot);
-  console.log(location.state);
+  const { state } = useLocation();
+
+  const spotData = state.filter((item) => item.tourDestNm === spot);
+  console.log(spotData);
 
   return (
     <div>
-      디테일한 장소에 대한 설명이 추가 될 페이지 입니다.
       <h1>{spot}</h1>
+      <hr />
+      <h2>주소지 : {spotData[0].addrJibun}</h2>
+
+      <p>{spotData[0].tourDestIntro}</p>
     </div>
   );
 };
